26c9bb8cf3b7630842d8aec208f10d33f61371d0
pcw109550 <pcw109550@gmail.com>
2023-06-01 13:35:05 +0900

    Add source for bashrc refresh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ sudo apt install -y gcc g++ tmux jq make
 wget https://go.dev/dl/go1.19.6.linux-arm64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.19.6.linux-arm64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+source ~/.bashrc
 
 # setup export
 cd 
