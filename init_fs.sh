43188c7eced989614d3533413974f5060cefd1e5
pcw109550 <pcw109550@gmail.com>
2023-06-06 12:07:25 +0900

    Fix device dir

--- init_fs.sh
+++ init_fs.sh
@@ -5,26 +5,28 @@ init_filesystem() {
     DEVICE=$1
     MOUNT_DIR=$2
 
+    DEVICE_DIR="/dev/$DEVICE"
+    
     sudo mkdir -p "$MOUNT_DIR"
     sudo chown -R $(whoami):$(whoami) "$MOUNT_DIR"
 
-    if ! lsblk -f "$DEVICE" | grep -q "$DEVICE"; then
+    if ! lsblk -f "$DEVICE_DIR" | grep -q "$DEVICE"; then
         echo "device $DEVICE does not exist"
         exit 1
     fi
 
-    if ! sudo file -s "$DEVICE" | grep -q "data"; then 
+    if ! sudo file -s "$DEVICE_DIR" | grep -q "data"; then 
         echo "device $DEVICE has fs initialized"
         exit 1
     fi
 
     # make file system
     # warning: volume data will be wiped
-    sudo mkfs -t xfs "$DEVICE"
+    sudo mkfs -t xfs "$DEVICE_DIR"
 
     # mount
-    sudo mount "$DEVICE" "$MOUNT_DIR"
+    sudo mount "$DEVICE_DIR" "$MOUNT_DIR"
 }
 
-init_filesystem "/dev/nvme1n1" "/data1"
-init_filesystem "/dev/nvme2n1" "/data2"
+init_filesystem "nvme1n1" "/data1"
+init_filesystem "nvme2n1" "/data2"
