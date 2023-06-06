dd3e1c8c18b5df3a20b35b5b273c014b5f42e027
pcw109550 <pcw109550@gmail.com>
2023-06-07 03:11:09 +0900

    Add slack reporter

---
+++ slack_report.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+OAUTH_TOKEN="<FIXME>"
+
+if [[ $# -eq 0 ]]; then
+    echo "Error: Message argument not provided."
+    # never fail
+    exit 0
+fi
+
+CHANNEL="#op-erigon-mainnet-bedrock-migration"
+MESSAGE="$1"
+
+# never fail
+curl -X POST -H "Authorization: Bearer $OAUTH_TOKEN" \
+     -H 'Content-type: application/json' \
+     -d "{'channel': '$CHANNEL', 'text': '$MESSAGE'}" \
+     https://slack.com/api/chat.postMessage || true
